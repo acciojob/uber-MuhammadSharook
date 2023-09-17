444b35e55d53e801b64f59ff53fe96f98f071487
Sharook <sk.sharook106@gmail.com>
2023-09-17 12:05:53 +0530

    1

---
+++ src/main/java/com/driver/model/Admin.java
@@ -0,0 +1,51 @@
+package com.driver.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Admin
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int adminID;
+    private String username;
+    private String password;
+
+    public Admin()
+    {
+
+    }
+
+    public Admin(int adminID, String username, String password) {
+        this.adminID = adminID;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(int adminID) {
+        this.adminID = adminID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
